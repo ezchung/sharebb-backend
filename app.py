4e92b9fa89bfd5e31c4f27cdc50ec6d40e54474c
Ezra Chung <ezra.chung97@gmail.com>
2022-12-06 14:53:41 -0800

    connecting with aws

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 s3 = boto3.client(
   "s3", # Needs to be S digit 3
   "us-west-1",
-  aws_access_key_id=os.environ["aws_access_key_id"],
-  aws_secret_access_key=os.environ["aws_secret_access_key"],
+  aws_access_key_id=os.environ['aws_access_key_id'],
+  aws_secret_access_key=os.environ['aws_secret_access_key'],
 )
 # IPython and try out to see if works
 # Method to list buckets, see if authenticating
+buckets = s3.list_buckets()['Buckets']
 
-buckets = s3.list_buckets()
 print(buckets)
 
 # upload_file(file_name, bucket, object_name)
